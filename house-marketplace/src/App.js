aa57619598cf576e384eeab419ce19868ad4e4db
Eduard <blademan82@gmail.com>
2022-07-18 20:13:22 +0300

    add nav component

--- house-marketplace/src/App.js
+++ house-marketplace/src/App.js
@@ -1,5 +1,28 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Explore from './pages/Explore';
+import Offers from './pages/Offers';
+import Profile from './pages/Profile';
+import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
+import Navbar from './nav/Navbar';
+import ForgotPassword from './pages/ForgotPassword';
+
 function App() {
-  return <div>start</div>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Explore />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+        </Routes>
+        <Navbar />
+      </Router>
+    </>
+  );
 }
 
 export default App;
